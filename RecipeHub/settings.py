b63cde8b7985a63dcfb594a9bbf30fd115377d9a
RanaTarekAhmed <r.tarekahmed@gmail.com>
2025-05-16 17:55:14 +0300

    Added multiple files related to AdminDashboard

--- RecipeHub/settings.py
+++ RecipeHub/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'recipe',
     'report',
     'start',
+    'categories',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom User Model
+AUTH_USER_MODEL = 'authorization.User'
--- RecipeHub/urls.py
+++ RecipeHub/urls.py
@@ -24,9 +24,12 @@ urlpatterns = [
     # Admin URL
     path('admin/', admin.site.urls),
     
+    # Start URLs (Dashboard and Recipe Management)
+    path('', include('start.urls')),
+    
     # Report URLs
     path('', include('report.urls')),
     
-    # Root URL - redirect to report page for now
-    path('', RedirectView.as_view(url='report/', permanent=False)),
+    # Root URL - redirect to dashboard page
+    path('', RedirectView.as_view(url='dashboard/', permanent=False)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
