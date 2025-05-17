79fef1f6bf44bc4f783f6cd41be1227b441f4c66
RanaTarekAhmed <r.tarekahmed@gmail.com>
2025-05-17 14:13:20 +0300

    admin dashboard

--- start/urls.py
+++ start/urls.py
@@ -1,16 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'start'
-
 urlpatterns = [
-#     path('admin/dashboard/', views.dashboard_view, name='admin_dashboard'),
-#     path('user/dashboard/', views.dashboard_view, name='user_dashboard'),
-#     path('recipes/', views.recipe_list, name='recipe_list'),
-#     path('users/', views.user_list, name='user_list'),
-#     path('categories/', views.category_list, name='category_list'),
-#     path('recipes/add/', views.add_recipe, name='add_recipe'),
-#     path('recipes/edit/', views.edit_recipe, name='edit_recipe'),  # List view
-#     path('recipes/edit/<int:id>/', views.edit_recipe, name='edit_recipe_detail'),  # Detail view
-#     path('api/recipes/<int:id>/', views.delete_recipe_api, name='delete_recipe_api'),
-]
+    path('', views.admin_dashboard, name='admin_dashboard'),
+    path('recipes/', views.admin_recipes, name='admin_recipes'),
+    path('recipe-list/', views.admin_recipe_list, name='admin_recipe_list'),
+    path('users/', views.admin_users, name='admin_users'),
+    path('add-recipe/', views.admin_add_recipe, name='admin_add_recipe'),
+    path('edit-recipe/<int:recipe_id>/', views.admin_edit_recipe, name='admin_edit_recipe'),
+] 
