8c7596818b1dcdf4e6f40a67ac447293f9e716f7
Aly-El-Badry <ali.el.badry.747@gmail.com>
2025-05-19 00:02:40 +0300

    Report activated

--- report/urls.py
+++ report/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('submit-report/', views.add_report, name='add_report'),
     path('view-reports/', views.view_reports_list, name="view_reports"),
+    path('view-report/<int:report_id>/', views.view_report_detail, name='view_report_detail'),
 ]
