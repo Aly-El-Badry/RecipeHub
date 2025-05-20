d7317c07fb2a9764114f048822f18b50f30b298e
Aly-El-Badry <ali.el.badry.747@gmail.com>
2025-05-20 23:04:38 +0300

    modificaiton

--- Interface/views.py
+++ Interface/views.py
@@ -94,8 +94,20 @@ def users(request):
     
 
 
+def handler400(request, exception):
+    return render(request, 'Interface/400.html', status=400)
+
+def handler403(request, exception):
+    return render(request, 'Interface/403.html', status=403)
+
 def handler404(request, exception):
     return render(request, 'Interface/404.html', status=404)
+
+def handler500(request):
+    return render(request, 'Interface/500.html', status=500)
+
+def handler503(request, exception):
+    return render(request, 'Interface/503.html', status=503)
 
 @login_required
 def delete_user(request, user_id):
